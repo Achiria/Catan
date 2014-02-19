008d90f1b1a9228acc40a4eb20fef51e1a38ffd4
Anthony Gamboa <anthony.b.gamboa@gmail.com>
2014-02-18 20:33:11 -0800

    Update Intersection.java

--- catan1/pkg1/Intersection.java
+++ catan1/pkg1/Intersection.java
@@ -15,7 +15,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-
 package catan1.pkg1;
 
 public class Intersection
@@ -23,44 +22,44 @@ public class Intersection
     private final Set location;
     private Boolean occupied;
     private Building type;
-    
+
     public Intersection()
     {
         location = new Set();
         occupied = false;
         type = null;
     }
-    
+
     public Intersection(int a, int b)
     {
-        location = new Set(a, b);     
+        location = new Set(a, b);
     }
-    
+
     public void setOcc(boolean toSet)
     {
         occupied = toSet;
     }
-    
+
     public void setType(Building built)
     {
         type = built;
     }
-    
+
     public Building getType()
     {
         return type;
     }
-    
+
     public int getX()
     {
         return location.x;
     }
-    
+
     public int getY()
     {
         return location.y;
     }
-    
+
 //    public boolean isValid()
 //    {
 //        if (occupied == true)

9fccf790991b33af3c0bea385b9b9a36ac8c8fcf
Anthony Gamboa <anthony.b.gamboa@gmail.com>
2014-02-18 20:30:29 -0800

    Update Building.java

--- catan1/pkg1/Building.java
+++ catan1/pkg1/Building.java
@@ -15,13 +15,11 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-
 package catan1.pkg1;
 
 public interface Building
-{           
+{
     public int getValue();
 
     public Player getOwner();
-            
 }

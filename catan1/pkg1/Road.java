0a9383d38283d9a5e23a65ec7502ad8b022ae280
Anthony Gamboa <anthony.b.gamboa@gmail.com>
2014-02-18 20:34:40 -0800

    Update Road.java

--- catan1/pkg1/Road.java
+++ catan1/pkg1/Road.java
@@ -15,18 +15,17 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-
 package catan1.pkg1;
 
 public class Road
 {
     private Player owner;
-    
+
     public Road(Player o)
     {
         owner = o;
     }
-    
+
     public Player getOwner()
     {
         return owner;

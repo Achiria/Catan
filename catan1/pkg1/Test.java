4980d64106e11b939be5ecc6595850949d1f6a2f
Anthony Gamboa <anthony.b.gamboa@gmail.com>
2014-06-23 20:26:01 -0700

    Update Test.java

--- catan1/pkg1/Test.java
+++ catan1/pkg1/Test.java
@@ -22,7 +22,7 @@ import java.util.concurrent.TimeUnit;
 
 public class Test
 {
-    static Player playerTurn;
+//    static Player playerTurn;
     static DrawTable gui = new DrawTable();
 
     static Player p1;
@@ -39,7 +39,120 @@ public class Test
 
         Board thingy = new Board();
 
-        Dialogs.turnChange(p1);
+        int[] rolls = new int[4];
+
+        Dialogs.positionRoll();
+
+        Dialogs.tellRoll(p1);
+        rolls[0] = Actions.roll();
+        Dialogs.tellRoll(p2);
+        rolls[1] = Actions.roll();
+        Dialogs.tellRoll(p3);
+        rolls[2] = Actions.roll();
+        Dialogs.tellRoll(p4);
+        rolls[3] = Actions.roll();
+
+        int biggestRoll = rolls[0];
+
+        for (int i = 1; i < rolls.length; i++)
+        {
+            if (rolls[i] > biggestRoll)
+            {
+                biggestRoll = rolls[i];
+            }
+        }
+
+        int startPlayer;
+        
+        if (biggestRoll == rolls[0])
+        {
+            Dialogs.firstRoll(p1);
+            Turn.turnSet(0);
+            startPlayer = 0;
+        }
+
+        else if (biggestRoll == rolls[1])
+        {
+            Dialogs.firstRoll(p2);
+            Turn.turnSet(1);
+            startPlayer = 1;
+        }
+
+        else if (biggestRoll == rolls[2])
+        {
+            Dialogs.firstRoll(p3);
+            Turn.turnSet(2);
+            startPlayer = 2;
+        }
+
+        else
+        {
+            Dialogs.firstRoll(p4);
+            Turn.turnSet(3);
+            startPlayer = 3;
+        }
+        
+        for (int i = 0; i < 4; i++)
+        {
+            p1.addHand(i, 2);
+            p2.addHand(i, 2);
+            p3.addHand(i, 2);
+            p4.addHand(i, 2);
+        }
+        
+        
+        
+        for (int i = startPlayer; i < 4; i++)
+        {
+            String input = Dialogs.buildPlace();
+            input = input.trim();
+            int xPoint = Integer.parseInt(input.substring(0, 1));
+            int yPoint = Integer.parseInt(input.substring(2, 3));
+            String inter = input.substring(4, input.length());
+            Tile t = Board.table[xPoint][yPoint];
+            Turn.getPlayer().buildSettlement(t.getX(), t.getY(), inter);
+            DrawTable.checkTable();
+            Turn.turnEnded();
+        }
+        
+        for (int i = 0; i < startPlayer; i++)
+        {
+            String input = Dialogs.buildPlace();
+            input = input.trim();
+            int xPoint = Integer.parseInt(input.substring(0, 1));
+            int yPoint = Integer.parseInt(input.substring(2, 3));
+            String inter = input.substring(4, input.length());
+            Tile t = Board.table[xPoint][yPoint];
+            Turn.getPlayer().buildSettlement(t.getX(), t.getY(), inter);
+            DrawTable.checkTable();
+            Turn.turnEnded();
+        }
+        
+        for (int i = startPlayer; i >= 0; i--)
+        {
+            String input = Dialogs.buildPlace();
+            input = input.trim();
+            int xPoint = Integer.parseInt(input.substring(0, 1));
+            int yPoint = Integer.parseInt(input.substring(2, 3));
+            String inter = input.substring(4, input.length());
+            Tile t = Board.table[xPoint][yPoint];
+            Turn.getPlayer().buildSettlement(t.getX(), t.getY(), inter);
+            DrawTable.checkTable();
+            Turn.turnEnded();
+        }
+        
+        for (int i = 3; i > startPlayer; i--)
+        {
+            String input = Dialogs.buildPlace();
+            input = input.trim();
+            int xPoint = Integer.parseInt(input.substring(0, 1));
+            int yPoint = Integer.parseInt(input.substring(2, 3));
+            String inter = input.substring(4, input.length());
+            Tile t = Board.table[xPoint][yPoint];
+            Turn.getPlayer().buildSettlement(t.getX(), t.getY(), inter);
+            DrawTable.checkTable();
+            Turn.turnEnded();
+        }
 
         while (p1.getPoints() < 10
                 && p2.getPoints() < 10
